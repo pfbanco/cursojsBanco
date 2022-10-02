b5b75c8bc739e8501deb489251524c179f99b625
pfbanco <104268562+pfbanco@users.noreply.github.com>
2022-10-02 13:55:44 -0300

    Update scipt.js
    Probando 2

--- script/scipt.js
+++ script/scipt.js
@@ -45,7 +45,7 @@ let tysis = new Pokemon('tysardo', 31, 'Veneno','Oscuridad','Porteñosis aguda',
 pokemons.push(bulbasaur, ivysaur, venasaur, charmander,charmeleon,charizard, squirtle, wartortle, blastoise, caterpie, metapod, butterfree, weedle, kakuna,beedrill, pidgey, pidgeotto, pidgeot, rattata, raticate, spearow, fearow, ekans, arbok, pikachu, raichu, sandshrew, sandslash ,nidoranH, nidorina, tysis)
 //localStorage.setItem("equipo", JSON.stringify(pokemons))
 let equipoDesdeJSON = []
-//let equipoDesdeJSON = JSON.parse(localStorage.getItem("equipo")) 
+equipoDesdeJSON = JSON.parse(localStorage.getItem("equipo")) 
 let idGlo = 0
 
 //DOM
